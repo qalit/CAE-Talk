1f27b2f05a5021aa47a1fb2a9fb6922c25f3a041
qalit <qal.smith@gmail.com>
2014-03-29 21:27:57 +0700

    commit listening socket

--- src/id/ac/unsyiah/jte/socket/EchoServerSederhana.java
+++ src/id/ac/unsyiah/jte/socket/EchoServerSederhana.java
@@ -8,17 +8,37 @@ package id.ac.unsyiah.jte.socket;
  *
  */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServerSederhana {
     public static void main(String[] args) throws Exception {
 
-        // pembuatan  socket baru pada server
+        //initialisasi port socket pada server
         int portSocket = 1805;
+        // pembuatan  socket baru pada server
         ServerSocket serverSocket = new ServerSocket(portSocket);
-        System.out.println("Memulai  " + serverSocket);
+        //Listening pada serversocket
+        System.out.println("Listening on Port " + 
+        			serverSocket.getLocalPort() +
+        			" " + serverSocket.getInetAddress());
         
-        Socket scoketTerima = serverSocket.accept();
+        //mencoba menerima socket dari client
+        try {
+        	Socket socketTerima = serverSocket.accept();
+        	BufferedReader masuk = new BufferedReader(
+        								new InputStreamReader(socketTerima.getInputStream()));
+        	String strMasuk = masuk.readLine();
+        	System.out.print("IP di Terima -> " + strMasuk);
+        	if ( strMasuk == "127.0.0.2"){
+        		System.out.print("IP di Tolak -> " + strMasuk);
+        	}
+        } finally {
+        	//jika selesai loop tutup socket yang terbuka
+        	serverSocket.close();
+        }
+        
     }
 }
